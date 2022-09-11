c17d3fdad4ad5621f3636c5692d124e4b74616f1
GeorgeNMJ <gnmoralesjr@gmail.com>
2022-09-12 07:11:43 +0800

    deleted unnecessary files

--- src/FaceDetect/FaceDetect.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-class FaceDetect {
-public:
-  static void fdetect(std::string path);
-};
--- src/Warp/Warp.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <opencv2/core/mat.hpp>
-#include <string>
-
-class Warp {
-
-public:
-  static void warp_image(std::string &path);
-};
